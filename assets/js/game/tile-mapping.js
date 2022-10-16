66e2acc6a449dfefd59b7e8bf2e44e6b985cb87a
MarkClyne95 <99036942+MarkClyne95@users.noreply.github.com>
2022-10-16 15:18:53 +0100

    Year 4 Update

--- assets/js/game/tile-mapping.js
+++
@@ -1,9 +0,0 @@
-let TILE_MAPPING = {
-    BLANK: 520,
-    DOOR: [407, 408, 447, 448, 487, 488, 854],
-    GRASS: 425,
-    CAVEENTRANCE: [568, 571, 1233, 199, 698, 2363, 2364],
-    CAVEGRASS: 7165
-};
-
-export default TILE_MAPPING;
